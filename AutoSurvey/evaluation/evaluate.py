9b343beb9c01bd98acd6b6462d4228c3576d8c22
Thales Rogério <thalesrogerio@gmail.com>
2023-06-13 19:10:47 -0300

    update evaluate to receive gold separately

--- AutoSurvey/evaluation/evaluate.py
+++ AutoSurvey/evaluation/evaluate.py
@@ -5,7 +5,7 @@ from settings import settings
 from textwrap import dedent
 
 
-def evaluate(dataset: str, log_file: str):
+def evaluate(dataset: str, gold_path, log_file: str):
     """
     Returns the average score for the dataset.
     Args:
@@ -16,13 +16,20 @@ def evaluate(dataset: str, log_file: str):
     """
     with open(dataset, "r") as f:
         data = json.load(f)
+
+    with open(gold_path, "r") as f:
+        gold_data = json.load(f)
     
     with open(log_file, "w") as f:
         all_scores = []
         for survey_name in data:
             for survey_section, content in data[survey_name].items():
-                generated_content = content["generated"]
-                ground_truth = content["gold"]
+                generated_content = content["content"]
+                ground_truth = gold_data[survey_name][survey_section]["content"]
+
+                if ground_truth == "":
+                    raise ValueError("Ground truth is empty")
+
                 prompt = (
                     f"{base_prompt}\nSurvey Name: {survey_name.strip()}\nSurvey Section: {survey_section.strip()}\nContent: {generated_content.strip()}\nGround Truth Text: {ground_truth}\nEvaluation Form (scores ONLY)\nScore:"
                 ) 
@@ -41,7 +48,7 @@ def get_llm_score(prompt):
     Please keep the document open while reviewing, and refer to it as needed.""")
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {"role": "system", "content": system_prompt.strip()},
             {"role": "user", "content": prompt},
@@ -56,10 +63,18 @@ def get_llm_score(prompt):
     scores = sum(all_predictions.count(i)/len(all_predictions) * i for i in range(1, 6))
     return scores
    
+# python evaluate.py --dataset /home/thales/Documents/AutoSurvey/test/proc1/proc1.json --gold /home/thales/Documents/AutoSurvey/data/dataset/survey_3.json --logs proc1_eval.json
 
+# python evaluate.py --dataset /home/thales/Documents/AutoSurvey/test/proc2/proc2.json --gold /home/thales/Documents/AutoSurvey/data/dataset/survey_3.json --logs proc2_eval.json
+
+# python evaluate.py --dataset /home/thales/Documents/AutoSurvey/test/proc3/proc3.json --gold /home/thales/Documents/AutoSurvey/data/dataset/survey_3.json --logs proc3_eval.json
+
+# python evaluate.py --dataset /home/thales/Documents/AutoSurvey/test/proc4/proc4.json --gold /home/thales/Documents/AutoSurvey/data/dataset/survey_3.json --logs proc4_eval.json
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--dataset", type=str, required=True, help="Path to the json dataset")
+    argparser.add_argument("--gold", type=str, required=True, help="Path to the json dataset")
+
     argparser.add_argument("--logs", type=str, default="evaluation_results.json", help="Path to save the evaluation results")
 
     args = argparser.parse_args()
@@ -71,4 +86,4 @@ if __name__ == "__main__":
     2 - Evaluate whether the content adequately addresses the main topic stated in the title and provides a comprehensive technical description of it.
     3 - Assign a score to the text on a scale of 1 to 5, where 1 represents the lowest score and 5 represents the highest score, according to the Evaluation Criteria.""")
 
-    average_score = evaluate(args.dataset, args.logs)
+    average_score = evaluate(args.dataset, args.gold, args.logs)
